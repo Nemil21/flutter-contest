b901d32e614f461e78546a42a0fe245434f9fbab
Nemil21 <143175683+Nemil21@users.noreply.github.com>
2024-03-22 00:02:28 +0530

    Update characterWidget.dart

--- lib/characterWidget.dart
+++ lib/characterWidget.dart
@@ -9,6 +9,7 @@ class characterWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+       color: Color.fromRGBO(18, 64, 118, 1),
       child: Column(
         children: [
           Padding(
@@ -18,39 +19,51 @@ class characterWidget extends StatelessWidget {
                 Text('->Id-' + char.id.toString(),
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
-                        color: Colors.indigo,
+                        color: Colors.white,
                         fontSize: 7.sp,
                         fontFamily: 'interTight')),
                 Text('-------',
                     style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 5.sp)),
+                        TextStyle(fontWeight: FontWeight.bold,color: Colors.white, fontSize: 5.sp)),
                 Text('Name -' + char.name,
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
-                        color: Colors.blue,
+                        color: Colors.white,
                         fontSize: 5.sp,
                         fontFamily: 'interTight'))
               ],
             ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: char.films.map((film) {
-              return ListTile(
-                contentPadding:
-                    EdgeInsets.fromLTRB(1,1,1,1),
-                leading: Icon(Icons.arrow_right),
-                title: Text(
-                  film,
+          ),Padding(
+            padding: const EdgeInsets.fromLTRB(10, 2, 10, 2),
+            child: Row(
+              children: [Text('Films - ',
                   style: TextStyle(
-                    fontFamily: 'interTight',
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromRGBO(34, 40, 49, 1),
-                    fontSize: 3.sp,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 3.sp))],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 2, 10, 2),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: char.films.map((film) {
+                return ListTile(
+                  contentPadding:
+                      EdgeInsets.fromLTRB(1,1,1,1),
+                  leading: Icon(Icons.arrow_right),
+                  title: Text(
+                    film,
+                    style: TextStyle(
+                      fontFamily: 'interTight',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 3.sp,
+                    ),
                   ),
-                ),
-              );
-            }).toList(),
+                );
+              }).toList(),
+            ),
           ),
 
           // Row(
@@ -60,68 +73,77 @@ class characterWidget extends StatelessWidget {
           //             color: Colors.amber,
           //             fontSize: 5.sp))],
           // ),
-          Row(
-            children: [
-              Text('Short Films - ',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromRGBO(49, 54, 63, 1),
-                      fontSize: 3.sp)),
-              if (char.shortFilms.isEmpty)
-                Text('No short films for this character',
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 2, 10, 2),
+            child: Row(
+              children: [
+                Text('Short Films - ',
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
-                        color: Colors.red,
+                        color: Colors.white,
                         fontSize: 3.sp)),
-              if (char.shortFilms.isNotEmpty)
-                Text('${char.shortFilms.join(', ')}',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.green,
-                        fontSize: 5.sp)),
-            ],
+                if (char.shortFilms.isEmpty)
+                  Text('No short films for this character',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red,
+                          fontSize: 3.sp)),
+                if (char.shortFilms.isNotEmpty)
+                  Text('${char.shortFilms.join(', ')}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 5.sp)),
+              ],
+            ),
           ),
-          Row(
-            children: [
-              Text('TV Shows - ',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromRGBO(118, 171, 174, 1),
-                      fontSize: 3.sp)),
-              if (char.tvShows.isEmpty)
-                Text('No short films for this character',
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 2, 10, 2),
+            child: Row(
+              children: [
+                Text('TV Shows - ',
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
-                        color: Colors.red,
+                        color: Colors.white,
                         fontSize: 3.sp)),
-              if (char.tvShows.isNotEmpty)
-                Text('${char.tvShows.join(', ')}',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromRGBO(118, 171, 174, 1),
-                        fontSize: 3.sp))
-            ],
+                if (char.tvShows.isEmpty)
+                  Text('No short films for this character',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red,
+                          fontSize: 3.sp)),
+                if (char.tvShows.isNotEmpty)
+                  Text('${char.tvShows.join(', ')}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 3.sp))
+              ],
+            ),
           ),
-          Row(
-            children: [
-              Text('Video Games - ',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromRGBO(23, 107, 135,1),
-                      fontSize: 3.sp)),
-              if (char.tvShows.isEmpty)
-                Text('No short films for this character',
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 2, 10, 2),
+            child: Row(
+              children: [
+                Text('Video Games - ',
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
-                        color: Colors.red,
+                        color: Colors.white,
                         fontSize: 3.sp)),
-              if (char.tvShows.isNotEmpty)
-                Text('${char.tvShows.join(', ')}',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromRGBO(118, 171, 174, 1),
-                        fontSize: 3.sp))
-            ],
+                if (char.tvShows.isEmpty)
+                  Text('No short films for this character',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red,
+                          fontSize: 3.sp)),
+                if (char.tvShows.isNotEmpty)
+                  Text('${char.tvShows.join(', ')}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 3.sp))
+              ],
+            ),
           ),
         ],
       ),
